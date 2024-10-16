eccc92b4ceb4df9ad19e2303c5e9e136ec93a1b4
shanggeeth <shanggeethk@gmail.com>
2024-10-16 17:53:39 +0530

    Add 200 concurrency

--- common/jmeter/perf-test-is.sh
+++ common/jmeter/perf-test-is.sh
@@ -218,6 +218,9 @@ heap_regex='^[0-9]+[MG]$'
 if [ "$concurrency" == "50-500" ]; then
     echo "Running tests for concurrency level 50-500"
     default_concurrent_users="50 100 150 300 500"
+elif [ "$concurrency" == "200" ]; then
+    echo "Running tests for concurrency level 200"
+    default_concurrent_users="200"
 elif [ "$concurrency" == "500-3000" ]; then
     echo "Running tests for concurrency level 500-3000"
     default_concurrent_users="500 1000 1500 2000 2500 3000"
